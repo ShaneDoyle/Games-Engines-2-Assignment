f2ee1ef06204bb58c2110041ab361b099d0f9620
ShaneDoyle <shanedoyle21@hotmail.com>
2019-03-12 00:46:40 +0000

    Commit 2
    Project assets uploaded.

---
+++ Assets/Scripts/SpinSpaceship.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinSpaceship : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, -0.15f);
+    }
+}
+
+
