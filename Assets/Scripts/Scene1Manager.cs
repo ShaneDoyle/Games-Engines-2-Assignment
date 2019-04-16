d27e8ad0443a78bb32365ffcd9f2d257851c3bed
ShaneDoyle <shanedoyle21@hotmail.com>
2019-04-16 17:12:04 +0100

    Commit 7
    Added support for scene changing.

---
+++ Assets/Scripts/Scene1Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Scene1Manager : MonoBehaviour
+{
+    //Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Scene2");
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Scene2()
+    {
+        yield return new WaitForSeconds(25);
+        SceneManager.LoadScene(1);
+    }
+}
