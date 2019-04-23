509726a4bd56db81ae61455ca17906b65b4e00ea
ShaneDoyle <shanedoyle21@hotmail.com>
2019-04-23 16:40:26 +0100

    Commit 12
    Added 2001 Waltz audio. Spaceship waltz scene is complete.

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -7,12 +7,17 @@ public class SceneController : MonoBehaviour
 {
     public float NextSceneWait = 0f;
     public int SceneNumber = 1;
+    private GameObject Audio = null;
 
     //Start is called before the first frame update
     void Start()
     {
         StartCoroutine("NextScene");
         SceneNumber--; //Adjust scene number to actually load the correct one.
+
+        //Find the music gameobject and don't delete when loading new scene. 
+        Audio = GameObject.FindGameObjectWithTag("Audio");
+        DontDestroyOnLoad(Audio);
     }
 
     //Update is called once per frame
