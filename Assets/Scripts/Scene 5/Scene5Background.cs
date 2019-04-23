6859bf4335510c04f128a13f853fdff7d4b2240c
ShaneDoyle <shanedoyle21@hotmail.com>
2019-04-23 15:52:41 +0100

    Commit 11
    Scene 6 complete.

---
+++ Assets/Scripts/Scene 5/Scene5Background.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene5Background : MonoBehaviour
+{
+    //Variable
+    private float xSpin = 0f;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(transform.up, 6 * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/Scripts/Scene 5/Scene5Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene5Camera : MonoBehaviour
+{
+    //Variables
+    public float CameraMovementSpeed = 0.001f;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, CameraMovementSpeed, Space.World);
+    }
+}
