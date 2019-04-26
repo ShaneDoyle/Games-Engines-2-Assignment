9d40398f00d74574baff4deccdab86e13ab001d7
ShaneDoyle <shanedoyle21@hotmail.com>
2019-04-26 23:18:37 +0100

    Commit 26
    Touched up scene 9, body now appears when at astronaut (Frank), plays sound to open pod bay doors.

--- Assets/EVAPod.cs
+++ Assets/EVAPod.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class EVAPod : MonoBehaviour
 {
     public GameObject Astronaut;
+    public GameObject EVAAstronaut;
+    public PathFollow PathScript;
+    public AudioSource PodBayDoors;
+
+
+    public int PathNum = 0;
+    private bool PlaySound = true;
+
     //Start is called before the first frame update
     void Start()
     {
@@ -14,9 +22,23 @@ public class EVAPod : MonoBehaviour
     //Update is called once per frame
     void Update()
     {
+        //Pathnum
+        PathNum = PathScript.pathnum;
+
+        if(PathNum >= 9)
+        {
+            if(PlaySound == true)
+            {
+                PlaySound = false;
+                PodBayDoors.Play();
+            }
+        }
+
+        //Remove Astronaut.
         if(Vector3.Distance(transform.position, Astronaut.transform.position) < 2)
         {
             Astronaut.SetActive(false);
+            EVAAstronaut.SetActive(true);
         }
     }
 }
