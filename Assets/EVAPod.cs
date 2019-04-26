3335ee2d19993eb1cce97262468adb761b33ccb5
ShaneDoyle <shanedoyle21@hotmail.com>
2019-04-26 22:52:10 +0100

    Commit 25
    Finished scene 9. Few touch ups needed with the body.

---
+++ Assets/EVAPod.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EVAPod : MonoBehaviour
+{
+    public GameObject Astronaut;
+    //Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, Astronaut.transform.position) < 2)
+        {
+            Astronaut.SetActive(false);
+        }
+    }
+}
